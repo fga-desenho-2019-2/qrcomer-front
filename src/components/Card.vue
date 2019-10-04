3066cba7cfdadf34cb2f1f135611860261cf05a9
phe0 <pheofo@gmail.com>
2019-10-03 23:36:57 -0300

    style cards components

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -15,8 +15,8 @@
             <img class="card-shopping__image" src="../assets/images/shopping-img.png">
             <div class="card-shopping__card">
                 <div class="card-shopping__card__text">
-                    <p class="card-shopping__card__text__title"></p>
-                    <p class="card-shopping__card__text__desc"></p>
+                    <p class="card-shopping__card__text__title">Você está no {{ title }}</p>
+                    <p class="card-shopping__card__text__desc">{{ description }}</p>
                 </div>
             </div>
         </div>
@@ -29,7 +29,7 @@ export default {
     props: {
         type: {
             type: String,
-            default: "shopping"
+            default: "restaurant"
         },
         title: {
             type: String,
@@ -37,7 +37,7 @@ export default {
         },
         description: {
             type: String,
-            default: "$ - Categoria"
+            default: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
         },
         orderTime: {
             type: String,
@@ -56,14 +56,15 @@ $title-size: 14px;
 $desc-size: 12px;
 
 .card-restaurant {
-    width: 45vw;
+    width: 44vw;
     max-width: 200px;
-    height: 45vw;
+    height: 44vw;
     max-height: 200px;
     background-color: #ffffff;
     border-radius: 0 15px;
     contain: content;
     box-shadow: 0 4px 10px $c-shadow;
+    margin: 10px;
 
     &__image {
         height: 45%;
@@ -105,36 +106,54 @@ $desc-size: 12px;
 }
 
 .card-shopping {
-    width: 95vw;
-    max-width: 450px;
-    height: 80vw;
-    max-height: 380px;
+    width: 100vw;
+    max-width: 1000px;
+    height: 95vw;
+    max-height: 400px;
     contain: content;
+    display: flex;
+    padding: 10px;
+    margin: auto;
   
     &__image {
         position: absolute;
         height: 50%;
-        right: 0;
+        right: 10px;
         z-index: 1;
     }
 
     &__card {
         width: 100%;
         height: 80%;
-        background-color: blue;
-        position: absolute;
+        background-color: #ffffff;
         bottom: 0;
         border-radius: 0px 0 0 15px;
         box-shadow: 0 4px 10px $c-shadow;
+        align-self: flex-end;
+        display: flex;
 
         &__text {
+            height: 50%;
+            width: 100%;
+            align-self: flex-end;
+            margin-top: 1.5em;
+            margin-right: 1em;
+            margin-bottom: 1em;
+            margin-left: 1em;
+            overflow: hidden;
+            text-overflow: ellipsis;
 
             &__title {
-
+                font-size: 16px;
+                padding-bottom: 2%;
+                contain: content;
+                font-weight: bold;
             }
 
             &__desc {
-
+                font-size: 12px;
+                color: $c-gray70;
+                word-wrap: break-word;
             }
         }
     }
--- src/views/ShoppingPage/ShoppingPage.vue
+++ src/views/ShoppingPage/ShoppingPage.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class='hola'>
-        <Card image='https://nit.pt/wp-content/uploads/2019/04/5179b21fc1d50950b99b4eecaa48c614-754x394.jpg' ></Card>
+    <div class="shopping">
+        <Card type="shopping" title="Shopping Park"  image='https://nit.pt/wp-content/uploads/2019/04/5179b21fc1d50950b99b4eecaa48c614-754x394.jpg' ></Card>
+        <div class="shopping__restaurants">
+            <Card image='https://nit.pt/wp-content/uploads/2019/04/5179b21fc1d50950b99b4eecaa48c614-754x394.jpg' title="Mcdonalds" description="$$ - Burguers" ></Card>
+            <Card image='https://nit.pt/wp-content/uploads/2019/04/5179b21fc1d50950b99b4eecaa48c614-754x394.jpg' title="Mcdonalds" description="$$ - Burguers"></Card>
+            <Card image='https://nit.pt/wp-content/uploads/2019/04/5179b21fc1d50950b99b4eecaa48c614-754x394.jpg' title="Mcdonalds" description="$$ - Burguers"></Card>
+            <Card image='https://nit.pt/wp-content/uploads/2019/04/5179b21fc1d50950b99b4eecaa48c614-754x394.jpg' title="Mcdonalds" description="$$ - Burguers"></Card>
+            <Card image='https://nit.pt/wp-content/uploads/2019/04/5179b21fc1d50950b99b4eecaa48c614-754x394.jpg' title="Mcdonalds" description="$$ - Burguers"></Card>
+        </div>
     </div>
 </template>
 
@@ -15,4 +22,14 @@ export default {
 </script>
 
 <style lang="scss">
+
+.shopping {
+    
+    &__restaurants {
+        flex-wrap: wrap;
+        display: flex;
+        justify-content: center;; 
+    }
+}
+
 </style>
