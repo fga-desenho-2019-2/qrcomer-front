8cfd659b4ee0a1dad25cb1d7abafaa20796b5ef5
phe0 <pheofo@gmail.com>
2019-10-03 22:20:45 -0300

    add shopping card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,24 @@
 <template>
-    <div class="card" >
-        <div class='card__image'>
-            <img class='card__image__content' :src='image' >
+    <div>
+        <div v-if="type === 'restaurant'" class="card-restaurant" >
+            <div class='card-restaurant__image'>
+                <img class='card-restaurant__image__content' :src='image' >
+            </div>
+            <div class="card-restaurant__content">
+                <p class='card-restaurant__content__title'>{{ title }}</p>
+                <p class='card-restaurant__content__desc'>{{ description }}</p>
+                <p class='card-restaurant__content__order-time'>{{ orderTime }}</p>
+            </div>
         </div>
-        <div class="card__content">
-            <p class='card__content__title'>{{ title }}</p>
-            <p class='card__content__desc'>{{ description }}</p>
-            <p class='card__content__order-time'>{{ orderTime }}</p>
+
+        <div v-if="type === 'shopping'" class="card-shopping" >
+            <img class="card-shopping__image" src="../assets/images/shopping-img.png">
+            <div class="card-shopping__card">
+                <div class="card-shopping__card__text">
+                    <p class="card-shopping__card__text__title"></p>
+                    <p class="card-shopping__card__text__desc"></p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -17,7 +29,7 @@ export default {
     props: {
         type: {
             type: String,
-            default: "restaurant"
+            default: "shopping"
         },
         title: {
             type: String,
@@ -43,13 +55,13 @@ export default {
 $title-size: 14px;
 $desc-size: 12px;
 
-.card {
+.card-restaurant {
     width: 45vw;
     max-width: 200px;
     height: 45vw;
     max-height: 200px;
-    background-color: $c-white;
-    border-radius: 0 15px 0 15px;
+    background-color: #ffffff;
+    border-radius: 0 15px;
     contain: content;
     box-shadow: 0 4px 10px $c-shadow;
 
@@ -88,6 +100,42 @@ $desc-size: 12px;
             font-size: $desc-size;
             color: $c-gray70;
             text-align: end;        
+        }
+    }
+}
+
+.card-shopping {
+    width: 95vw;
+    max-width: 450px;
+    height: 80vw;
+    max-height: 380px;
+    contain: content;
+  
+    &__image {
+        position: absolute;
+        height: 50%;
+        right: 0;
+        z-index: 1;
+    }
+
+    &__card {
+        width: 100%;
+        height: 80%;
+        background-color: blue;
+        position: absolute;
+        bottom: 0;
+        border-radius: 0px 0 0 15px;
+        box-shadow: 0 4px 10px $c-shadow;
+
+        &__text {
+
+            &__title {
+
+            }
+
+            &__desc {
+
+            }
         }
     }
 }
