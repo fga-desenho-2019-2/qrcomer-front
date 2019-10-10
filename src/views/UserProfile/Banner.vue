7cb806664618588f58fb2712040fe138f551a6ec
MatheusBlanco <msallesblanco@gmail.com>
2019-10-10 18:54:52 -0300

    Fixing responsiveness and buttons spacing in user page

--- src/views/UserProfile/Banner.vue
+++ src/views/UserProfile/Banner.vue
@@ -1,78 +1,78 @@
 <template>
-    <section id="banner" class="banner">
-        <v-container>
-            <v-row>
-                <v-col cols=12 sm=12 md=12 lg=12 class="col-banner">
-                    <div class="card-usuario-section">
-                        
-                        <QrcUsuario
-                            v-for="usuario in cardusuario"
-                            :key="usuario.id"
-                            :class="`usuario-${usuario.id}`"
-                            :img="usuario.img"
-                            :name="usuario.name"/>
-                    </div>
-                </v-col>
-            </v-row>
-        </v-container>
-    </section>
+  <section id="banner" class="banner">
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="12" md="12" lg="12" class="col-banner">
+          <div class="card-usuario-section">
+            <QrcUsuario
+              v-for="usuario in cardusuario"
+              :key="usuario.id"
+              :class="`usuario-${usuario.id}`"
+              :img="usuario.img"
+              :name="usuario.name"
+            />
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
 </template>
 
 <script>
-import Usuario from '../../components/Usuario.vue'
+import Usuario from "../../components/Usuario.vue";
 
 export default {
-    data() {
-        return {
-            cardusuario: [
-                {
-                    id:1,
-                    name: 'Blank',
-                    img: require('@/assets/images/profile/blank-profile-picture-.png')
-                },
-            ]
+  data() {
+    return {
+      cardusuario: [
+        {
+          id: 1,
+          name: "Blank",
+          img: require("@/assets/images/profile/blank-profile-picture-.png")
         }
-    },
-    components: {
-        'QrcUsuario': Usuario,
-    }
-}
+      ]
+    };
+  },
+  components: {
+    QrcUsuario: Usuario
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 #banner {
-        margin:0;
-        min-height: 45vh;
-        background: linear-gradient(to bottom right, transparent 50%, #fafafa 40%), linear-gradient(40deg, #eb4476, #e18855) #fafafa;
-        background-size:100% auto;
-        transition:0.5s;
+  margin: 0;
+  min-height: 45vh;
+  background: linear-gradient(to bottom right, transparent 50%, #fafafa 40%),
+    linear-gradient(-40deg, #eb4476, #e18855) #fafafa;
+  background-size: 100% auto;
+  transition: 0.5s;
 
-    .banner {
-        &--title {
-            font-size: 48px;
-        }
-        &--subtitle {
-            font-size: 36px;
-            line-height: 36px;
-        }
+  .banner {
+    &--title {
+      font-size: 48px;
     }
-    .col-banner {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
+    &--subtitle {
+      font-size: 36px;
+      line-height: 36px;
+    }
+  }
+  .col-banner {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 
-        &--img {
-            width: 50vw;
-            height: 200px;
-            margin-bottom: 5vh;
-            filter: drop-shadow(0px 4px 4px $c-gray70)
-        }
+    &--img {
+      width: 50vw;
+      height: 200px;
+      margin-bottom: 5vh;
+      filter: drop-shadow(0px 4px 4px $c-gray70);
     }
+  }
 
   /* extra test*/
-    &:hover {
-        background-size: 125% auto;/* add some extra tunning to deg direction and size ?*/
-    }
-
+  &:hover {
+    background-size: 125% auto; /* add some extra tunning to deg direction and size ?*/
+  }
 }
 </style>
--- src/views/UserProfile/User.vue
+++ src/views/UserProfile/User.vue
@@ -1,135 +1,121 @@
 <template>
-    <v-content class="d-flex flex-column pt-0">
-        <div>
-            <QrcBanner/>
-            <QrcCardUsuario/>
-              <v-card
-                class="mx-auto"
-                max-width="350"
-              >
-                <v-card-text>
-                  <p class="display-1 text--accent">
-                    Nome
-                  </p>
-                </v-card-text>
-              </v-card>
-              <br />
-              <v-card
-                class="mx-auto"
-                max-width="350"
-              >
-                <v-card-text>
-                  <p class="display-1 text--accent">
-                    email@email.com
-                  </p>
-                </v-card-text>
-              </v-card>
-              <br />
-              <v-card
-                class="mx-auto"
-                max-width="350"
-              >
-                <v-card-text>
-                  <p class="display-1 text--accent">
-                    (XX) XXXXX-XXXX
-                  </p>
-                </v-card-text>
-              </v-card>
-              <br />
-              <v-card
-                class="mx-auto"
-                max-width="350"
-              >
-                <v-card-text>
-                  <p class="display-1 text--accent">
-                    XXX.XXX.XXX-XX
-                  </p>
-                </v-card-text>
-              </v-card>
-              <br />
-                <v-row align="center">
-                    <v-row justify="space-around">
+  <v-content class="d-flex flex-column pt-0">
+    <div>
+      <QrcBanner />
+      <QrcCardUsuario />
+      <v-card class="mx-auto" max-width="350">
+        <v-card-text>
+          <p class="display-1 text--accent">Nome</p>
+        </v-card-text>
+      </v-card>
+      <br />
+      <v-card class="mx-auto" max-width="350">
+        <v-card-text>
+          <p class="display-1 text--accent">email@email.com</p>
+        </v-card-text>
+      </v-card>
+      <br />
+      <v-card class="mx-auto" max-width="350">
+        <v-card-text>
+          <p class="display-1 text--accent">(XX) XXXXX-XXXX</p>
+        </v-card-text>
+      </v-card>
+      <br />
+      <v-card class="mx-auto" max-width="350">
+        <v-card-text>
+          <p class="display-1 text--accent">XXX.XXX.XXX-XX</p>
+        </v-card-text>
+      </v-card>
+      <br />
 
-                    <v-btn
-                        color="success"
-                        class="qrc-btn primary my-2 "
-                        @click="validate"
-                    >
-                        <a href="./edit-user" style="text-decoration:none">Editar dados</a>
-                    </v-btn>
+      <v-col cols="12" justify-content="space-between">
+        <v-row justify="center">
+          <v-btn color="success" class="qrc-btn primary my-2">
+            <a href="./edit-user" class="a">Editar dados</a>
+          </v-btn>
 
-                      <v-dialog v-model="dialog" persistent max-width="290">
-                        <template v-slot:activator="{ on }">
-                          <v-btn color="success"
-                        class="qrc-btn primary my-2 " v-on="on">Apagar conta</v-btn>
-                          </template>
-                          <v-card>
-                            <v-card-title class="headline">Apagar a conta?</v-card-title>
-                              <v-card-text>Se a conta for apagada, registros de compras serão deletados. Esta não poderá ser recuperada.</v-card-text>
-                                <v-card-actions>
-                                  <div class="flex-grow-1"></div>
-                                  <v-btn color="success"
-                        class="qrc-btn primary my-2 "  @click=none>Cancelar</v-btn>
-                                  <v-btn color="success"
-                        class="qrc-btn primary my-2 " @click=vueDelete>Confirmar</v-btn>
-                                </v-card-actions>
-                              </v-card>
-                            </v-dialog>
-                          </v-row>
-                    </v-row>
-          </div>
-        <QrcFooter/>
-    </v-content>
+          <div class="divider"></div>
+
+          <v-dialog v-model="dialog" persistent max-width="290">
+            <template v-slot:activator="{ on }">
+              <v-btn color="success" class="qrc-btn primary my-2" v-on="on">Apagar conta</v-btn>
+            </template>
+
+            <v-card>
+              <v-card-title class="headline">Apagar a conta?</v-card-title>
+              <v-card-text>Se a conta for apagada, registros de compras serão deletados. Esta não poderá ser recuperada.</v-card-text>
+              <v-card-actions>
+                <div class="flex-grow-1"></div>
+                <v-btn color="success" class="qrc-btn primary my-2" @click="none">Cancelar</v-btn>
+                <v-btn color="success" class="qrc-btn primary my-2" @click="vueDelete">Confirmar</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-row>
+      </v-col>
+    </div>
+    <QrcFooter />
+  </v-content>
 </template>
 
 <script>
-import Banner from './Banner.vue'
-import CardUsuario from './CardUsuario.vue'
-import Footer from '../LandingPage/Footer.vue'
+import Banner from "./Banner.vue";
+import CardUsuario from "./CardUsuario.vue";
+import Footer from "../LandingPage/Footer.vue";
 
 export default {
-    components: {
-        "QrcBanner": Banner,
-        "QrcFooter": Footer,
-        "QrcCardUsuario": CardUsuario,
-    },
-    // data: () => ({
-    //   valid: true,
-    //   name: '',
-    //   email: '',
-    //   password: '',
-    //   telephone: '',
-    //   cpf: '',
-    // }),
+  components: {
+    QrcBanner: Banner,
+    QrcFooter: Footer,
+    QrcCardUsuario: CardUsuario
+  },
+  // data: () => ({
+  //   valid: true,
+  //   name: '',
+  //   email: '',
+  //   password: '',
+  //   telephone: '',
+  //   cpf: '',
+  // }),
 
-    data () {
-      return {
-        dialog:false,
+  data() {
+    return {
+      dialog: false
+    };
+  },
+  methods: {
+    edit() {
+      if (this.$refs.form.edit()) {
+        this.snackbar = true;
       }
     },
-    methods: {
-      edit () {
-        if (this.$refs.form.edit()) {
-          this.snackbar = true
-        }
-      },
-      vueDelete () {
-        this.$refs.delete()
-      },
-    },
-}
+    vueDelete() {
+      this.$refs.delete();
+    }
+  }
+};
 </script>
 
 <style lang="scss">
 .v-content__wrap {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
-.form{
+.a {
+  color: white;
+  text-decoration: none;
+}
+
+.form {
   justify-content: space-around;
   margin-left: 3%;
   margin-right: 3%;
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
+}
+
+.divider {
+  width: 30px;
 }
 </style>
