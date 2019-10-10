037728a60f8964784c5bbf2328c65eed305336cb
MatheusBlanco <msallesblanco@gmail.com>
2019-10-10 20:30:14 -0300

    Fixing editUser page responsiveness and forms sizes

--- src/views/EditUserProfile/EditUser.vue
+++ src/views/EditUserProfile/EditUser.vue
@@ -1,146 +1,117 @@
 <template>
-    <v-content class="d-flex flex-column pt-0">
-        <div>
-            <QrcBanner/>
-            <QrcEditUsuario/>
-            <v-row align="center">
-                    <v-row justify="space-around">
-                    <v-form
-                    ref="form"
-                    v-model="valid"
-                    >
-                    <v-text-field
-                        v-model="name"
-                        :counter="50"
-                        :rules="nameRules"
-                        label="Nome"
-                        required
-                    ></v-text-field>
+  <v-content class="d-flex flex-column pt-0">
+    <div>
+      <QrcBanner />
+      <QrcEditUsuario />
 
-                    <v-text-field
-                        v-model="email"
-                        :rules="emailRules"
-                        label="E-mail"
-                        required
-                    ></v-text-field>
+      <v-form v-model="valid">
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-text-field v-model="name" :rules="nameRules" label="Nome" required></v-text-field>
+            </v-col>
 
-                        <v-text-field
-                        v-model="password"
-                        :rules="passwordRules"
-                        label="Senha"
-                        required
-                    ></v-text-field>
+            <v-col cols="12" md="6">
+              <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+            </v-col>
 
-                    <v-text-field
-                        v-model="telephone"
-                        :rules="telephoneRules"
-                        label="Telefone"
-                        required
-                    ></v-text-field>
+            <v-col cols="12" md="6">
+              <v-text-field v-model="password" :rules="passwordRules" label="Senha" required></v-text-field>
+            </v-col>
 
-                    <v-text-field
-                        v-model="cpf"
-                        :rules="cpfRules"
-                        label="CPF"
-                        required
-                    ></v-text-field>
+            <v-col cols="12" md="6">
+              <v-text-field v-model="telephone" :rules="telephoneRules" label="Telefone" required></v-text-field>
+            </v-col>
 
-                    <v-btn
-                        :disabled="!valid"
-                        color="success"
-                        class="qrc-btn primary my-2 "
-                        @click="validate"
-                    >
-                        Confirmar
-                    </v-btn>
+            <v-col cols="12" md="6">
+              <v-text-field v-model="cpf" :rules="cpfRules" label="CPF" required></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-form>
+      <v-col cols="12" justify-content="space-between">
+        <v-row justify="center">
+          <v-btn
+            :disabled="!valid"
+            color="success"
+            class="qrc-btn primary my-2"
+            @click="validate"
+          >Confirmar</v-btn>
 
-                    <v-btn
-                        color="error"
-                        class="qrc-btn primary my-2 "
-                        @click="reset"
-                    >
-                        Apagar dados
-                    </v-btn>
+          <div class="divider"></div>
 
-
-                    </v-form>
-                    </v-row>
-                </v-row>
-        </div>
-            <QrcFooter/>
-    </v-content>
+          <v-btn color="error" class="qrc-btn primary my-2" @click="reset">Apagar dados</v-btn>
+        </v-row>
+      </v-col>
+    </div>
+    <QrcFooter />
+  </v-content>
 </template>
 
 <script>
-import Footer from '../LandingPage/Footer.vue'
-import Banner from './Banner.vue'
-import EditUsuario from './EditUsuario.vue'
+import Footer from "../LandingPage/Footer.vue";
+import Banner from "./Banner.vue";
+import EditUsuario from "./EditUsuario.vue";
 
 export default {
-    components: {
-        "QrcBanner": Banner,
-        "QrcFooter": Footer,
-        "QrcEditUsuario": EditUsuario,
-    },
-    data: () => ({
-      valid: true,
-      name: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 50) || 'Name must be less than 10 characters',
-      ],
-      email: '',
-      emailRules: [
-          v => !!v || 'E-mail is required',
-          v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-      ],
-      password: '',
-      passwordRules: [
-        v => !!v || 'Password is required',
-        v => (v && v.length >= 8) || 'Password must be bigger than 8 characters',
-      ],
-      telephone: '',
-      telefoneRules: [
-        v => !!v || 'Telephone is required',
-        v => /.+@.+\..+/.test(v) || 'Telephone must be valid',
-      ],
-      cpf: '',
-      cpfRules: [
-        v => !!v || 'CPF is required',
-        v => /.+@.+\..+/.test(v) || 'CPF must be valid',
-      ],
-    }),
+  components: {
+    QrcBanner: Banner,
+    QrcFooter: Footer,
+    QrcEditUsuario: EditUsuario
+  },
+  data: () => ({
+    valid: true,
+    name: "",
+    nameRules: [v => !!v || "Campo obrigatório"],
+    email: "",
+    emailRules: [
+      v => !!v || "Campo obrigatório",
+      v => /.+@.+\..+/.test(v) || "E-mail deve ser válido"
+    ],
+    password: "",
+    passwordRules: [
+      v => !!v || "Campo obrigatório",
+      v => (v && v.length >= 8) || "Senha deve ser maior do que 8 caracteres"
+    ],
+    telephone: "",
+    telephoneRules: [v => !!v || "Campo obrigatório"],
+    cpf: "",
+    cpfRules: [v => !!v || "Campo obrigatório"]
+  }),
 
-    methods: {
-      validate () {
-        if (this.$refs.form.validate()) {
-          this.snackbar = true
-        }
-      },
-      reset () {
-        this.$refs.form.reset()
-      },
+  methods: {
+    validate() {
+      if (this.$refs.form.validate()) {
+        this.snackbar = true;
+      }
     },
-}
+    reset() {
+      this.$refs.form.reset();
+    }
+  }
+};
 </script>
 
 <style lang="scss">
 .v-content__wrap {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
- #card-usuario {
-        padding: 40px;
-    }
+#card-usuario {
+  padding: 40px;
+}
 
-    .card-usuario-section {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        margin-right: -10px;
-        margin-left: -10px;
-        align-items: center;
-    }
+.card-usuario-section {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-right: -10px;
+  margin-left: -10px;
+  align-items: center;
+}
 
+.divider {
+  width: 30px;
+}
 </style>
