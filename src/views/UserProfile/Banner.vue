9a7f85751dbaf1f6b0baf95d71c0e17e3567f30e
Shayane Alcantara <shayaneliebe@gmail.com>
2019-10-13 13:15:53 -0300

    Remove useless style

--- src/views/UserProfile/Banner.vue
+++ src/views/UserProfile/Banner.vue
@@ -1,6 +1,6 @@
 <template>
-  <section id="banner" class="banner">
-    <v-container>
+  <section id="banner">
+    <v-container class="banner">
       <v-row>
         <v-col cols="12" sm="12" md="12" lg="12" class="col-banner">
           <div class="card-membro-section">
@@ -52,15 +52,6 @@ export default {
   background-size: 125% auto;
   transition: 0.5s;
 
-  .banner {
-    &--title {
-      font-size: 48px;
-    }
-    &--subtitle {
-      font-size: 36px;
-      line-height: 36px;
-    }
-  }
   .col-banner {
     display: flex;
     flex-direction: column;
