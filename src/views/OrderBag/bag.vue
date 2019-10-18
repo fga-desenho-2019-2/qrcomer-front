f5d5362e8578c08c2e79963eae3354a6850aa1ca
Pedro Rodrigues <peddroprp@gmail.com>
2019-10-18 16:10:44 -0300

    create payment card
    Co-authored-by: Matheus Blanco <msallesblanco@gmail.com>

--- src/views/OrderBag/bag.vue
+++ src/views/OrderBag/bag.vue
@@ -1,41 +1,53 @@
 <template>
     <v-content class="d-flex flex-column pt-0">
-        <div class="shopping-info">
-            <div class="shopping-info__image-area">
-                <img class="shopping-info__image-area__image" src="@/assets/images/place.svg"/>
+        <div class="card">
+            <div class="card__image-area">
+                <img class="card__image-area__image" src="@/assets/images/place.svg"/>
             </div>
-            <div class="shopping-info__text-area">
-                <div class="shopping-info__text-area__text">
+            <div class="card__text-area">
+                <div class="card__text-area__text">
                     <p id="shopping-name">Você ta no shopping do seu zé</p>
                     <p id="localization">Av josé palmeiras</p>
                 </div>
-                <div class="shopping-info__text-area__button">
+                <div class="card__text-area__button">
                     <a href="#">Não, estou em outro lugar</a>
                 </div>
             </div>
         </div>
-        <div class="shopping-info" id="foods">
-            <div class="shopping-info__restaurant" id="items">
+        <div class="card" id="foods">
+            <div class="card__restaurant" id="items">
                 <h5 id="restaurant-title">Mc Donnald's</h5>
                 <p>de 30 a 40 min</p>
-                <restaurantitem />
-                <restaurantitem />
+                <restaurantItem />
+                <restaurantItem />
 
             </div>
-            <div class="shopping-info__price">
+            <div class="card__price">
                 <p><a href="#">Adicionar mais itens</a></p>
-                <div class="shopping-info__price__total">
-                    <div class="shopping-info__price__total__text">
+                <div class="card__price__total">
+                    <div class="card__price__total__text">
                         <p>Total</p>
                     </div>
-                    <div class="shopping-info__price__total__number">
+                    <div class="card__price__total__number">
                         <p>R$ 29,40</p>
                     </div>
                 </div>
             </div>
         </div>
-
-
+        <div class="card" id="payment">
+            <div class="card__payment-method">
+                <div class="card__payment-method__title">
+                    <p class="mb-0">Forma de Pagamento</p>
+                </div>
+                <div class="card__payment-method__credit">
+                    <p>Cartão no app <br> ****3387</p>
+                    <a href="#" class="mb-0 mt-0">ALTERAR</a>
+                </div>
+                <div class="card__payment-method__cpf">
+                    <a href="#">Adicionar CPF</a>
+                </div>
+            </div>
+        </div>
         
         <v-bottom-navigation  color="white">
             <v-btn><font color="white">Finalizar Pedido - Total: </font></v-btn>
@@ -45,10 +57,10 @@
 </template>
 
 <script>
-    import restaurantitem from "@/components/BagItem.vue";
+    import restaurantItem from "@/components/BagItem.vue";
     export default {
         components:{
-            restaurantitem
+            restaurantItem
         }
     };
 </script>
@@ -59,7 +71,7 @@
     flex-direction: column;
 }
 
-.shopping-info {
+.card {
     width: 100%;
     background-color: $c-white;
     box-shadow: 0px 4px 10px #00000033;
@@ -131,6 +143,54 @@
             font-weight: normal;
         }
     }
+
+    &__payment-method {
+        width: 100%;
+        padding-left: 10px;
+
+
+        &__title {
+            font-size: 18px;
+            font-weight: normal;
+            border-bottom: 1px solid #C7C7C7;
+        }
+
+        &__credit {
+            display: flex;
+            justify-content: space-between;
+            padding-right: 10px;
+
+            p {
+                font-size: 12px;
+                color: #797979;
+                padding-top: 12px;
+            }
+
+            a:visited, a:link{
+                padding-top: 12px;
+                text-decoration: none;
+                color: #000000;
+            }
+            border-bottom: 1px solid#C7C7C7;
+        }
+
+        &__cpf {
+            padding-top: 12px;
+            padding-bottom: 12px;
+
+            a:link, a:visited {
+                text-decoration: none;
+                font-size: 12px;
+                color: #797979;
+            }
+        }
+    }
+}
+
+.v-bottom-navigation{
+    position: fixed;
+    bottom: 0;
+    left: 0;
 }
 
 #shopping-name {
@@ -149,5 +209,9 @@
 
 #foods{
     padding-top: 10px;
+}
+
+#payment{
+    padding-top: 5px;
 }
 </style>
