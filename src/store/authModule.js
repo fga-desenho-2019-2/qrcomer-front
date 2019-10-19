f68a7b95e77f7fd324a5089d41b5054e150c97f1
Elias Bernardo <ebmm01@gmail.com>
2019-10-18 23:06:09 -0300

    Adding auth Store

---
+++ src/store/authModule.js
@@ -0,0 +1,58 @@
+import axios from 'axios'
+
+const authModule = {
+    namespaced: true,
+    state: { 
+        token: localStorage.getItem('user-token') || '',
+        status: '',
+    },
+    mutations: {
+        AUTH_REQUEST: (state) => {
+            state.status = 'loading'
+        },
+        AUTH_SUCCESS: (state, token) => {
+            state.status = 'success'
+            state.token = token
+            console.log('commit nas mutations', state.status)
+        },
+        AUTH_ERROR: (state) => {
+            state.status = 'error'
+        },
+    },
+    actions: {
+        AUTH_REQUEST: ({commit, dispatch}, user) => {
+            return new Promise((resolve, reject) => { 
+                commit('AUTH_REQUEST')
+                axios({url: 'http://localhost:8000/api/token/', data: user, method: 'POST' })
+                    .then(resp => {
+                        const token = resp.data.access
+                        localStorage.setItem('user-token', token) // store the token in localstorage
+                        axios.defaults.headers.common['Authorization'] = token
+                        commit('AUTH_SUCCESS', token)
+                        // dispatch(USER_REQUEST)
+                        resolve(resp)
+                    })
+                .catch(err => {
+                    commit('AUTH_ERROR', err)
+                    localStorage.removeItem('user-token') // if the request fails, remove any possible user token if possible
+                    reject(err)
+                })
+            })
+        },
+        AUTH_LOGOUT: ({commit, dispatch}) => {
+            return new Promise((resolve, reject) => {
+                commit('AUTH_LOGOUT')
+                localStorage.removeItem('user-token') // clear your user's token from localstorage
+                // remove the axios default header
+                delete axios.defaults.headers.common['Authorization']
+                resolve()
+            })
+        }
+    },
+    getters: {
+        isAuthenticated: state => !!state.token,
+        authStatus: state => state.status,
+    }
+}
+
+export default authModule
