4b35d1e15920c8e84feba54c9b5ecac27091e661
MatheusBlanco <msallesblanco@gmail.com>
2019-10-19 21:13:40 -0300

    Adding logic to access shopping through cnpj

--- src/views/OrderBag/orderBagPage.vue
+++ src/views/OrderBag/orderBagPage.vue
@@ -1,273 +1,306 @@
 <template>
-    <v-content class="order-bag d-flex flex-column pt-0">
-        <div class="card">
-            <div class="card__image-area">
-                <img class="card__image-area__image" src="@/assets/images/place.svg"/>
-            </div>
-            <div class="card__text-area">
-                <div class="card__text-area__text">
-                    <p id="shopping-name">Você está no {{ shopping.name }}</p>
-                    <p id="localization">{{ shopping.address }}</p>
-                </div>
-                <div class="card__text-area__button">
-                    <a href="#">Não, estou em outro lugar</a>
-                </div>
-            </div>
+  <v-content class="order-bag d-flex flex-column pt-0">
+    <div class="card">
+      <div class="card__image-area">
+        <img class="card__image-area__image" src="@/assets/images/place.svg" />
+      </div>
+      <div class="card__text-area">
+        <div class="card__text-area__text">
+          <p id="shopping-name">Você está no {{ shopping.name }}</p>
+          <p id="localization">{{ shopping.address }}</p>
         </div>
-        <div class="card" id="foods">
-            <div class="card__restaurant" id="items">
-                <h5 class="mb-0" id="restaurant-title">{{ restaurant.name }}</h5>
-                <p>{{ restaurant.time }}min</p>
-                <restaurantItem v-for="(item, index) in items" :key="index" :name="item.name" :ammount="item.ammount" @changeQtd="handleAmmount($event, index)" />
-            </div>
+        <div class="card__text-area__button">
+          <a href="#">Não, estou em outro lugar</a>
+        </div>
+      </div>
+    </div>
+    <div class="card" id="foods">
+      <div class="card__restaurant" id="items">
+        <h5 class="mb-0" id="restaurant-title">{{ restaurant.name }}</h5>
+        <p>{{ restaurant.time }}min</p>
+        <restaurantItem
+          v-for="(item, index) in items"
+          :key="index"
+          :name="item.name"
+          :ammount="item.ammount"
+          @changeQtd="handleAmmount($event, index)"
+        />
+      </div>
 
-            <div class="card__price">
-                <p ><a class="palanquin" href="#"><b>Adicionar mais itens</b></a></p>
-                <div class="card__price__total">
-                    <div class="card__price__total__text">
-                        <p class="palanquin">Total</p>
-                    </div>
-                    <div class="card__price__total__number">
-                        <p class="palanquin">R${{ total }}</p>
-                    </div>
-                </div>
-            </div>
+      <div class="card__price">
+        <p>
+          <a class="palanquin" v-bind:href="'/shopping/' +shopping.cnpj">
+            <b>Adicionar mais itens</b>
+          </a>
+        </p>
+        <div class="card__price__total">
+          <div class="card__price__total__text">
+            <p class="palanquin">Total</p>
+          </div>
+          <div class="card__price__total__number">
+            <p class="palanquin">R${{ total }}</p>
+          </div>
         </div>
-        <div class="card" id="payment">
-            <div class="card__payment-method">
-                <div class="card__payment-method__title">
-                    <h6 class="mb-2">Forma de Pagamento</h6>
-                </div>
-                <div class="card__payment-method__credit">
-                    <p>Cartão no app <br> ****3387</p>
-                    <a href="#" class="card__payment-method__credit__link mb-0 mt-0 ">ALTERAR</a>
-                </div>
-                <div class="card__payment-method__cpf">
-                    <a href="#">Adicionar CPF</a>
-                </div>
-            </div>
+      </div>
+    </div>
+    <div class="card" id="payment">
+      <div class="card__payment-method">
+        <div class="card__payment-method__title">
+          <h6 class="mb-2">Forma de Pagamento</h6>
         </div>
-        
-        <v-bottom-navigation color="white">
-            <v-btn><font color="white">Finalizar Pedido - Total: </font></v-btn>
-        </v-bottom-navigation>
-    </v-content>
+        <div class="card__payment-method__credit">
+          <p>
+            Cartão no app
+            <br />****3387
+          </p>
+          <a href="#" class="card__payment-method__credit__link mb-0 mt-0">ALTERAR</a>
+        </div>
+        <div class="card__payment-method__cpf">
+          <a href="#">Adicionar CPF</a>
+        </div>
+      </div>
+    </div>
 
+    <v-bottom-navigation color="white">
+      <v-btn>
+        <font color="white">Finalizar Pedido - Total:</font>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-content>
 </template>
 
 <script>
-    import restaurantItem from "./BagItem.vue";
-    export default {
-        components:{
-            restaurantItem
-        },
-        data () {
-            return {
-                shopping: {},
-                restaurant: {},
-                items: []
-            }
-        },
-        created() {
-            this.getShopping();
-            this.getRestaurant();
-            this.getItems();
-        },
-        computed: {
-            total: function() {
-                let sum = 0;
-                if(this.items){
-                    this.items.forEach(item => {
-                        sum += item.value * item.ammount;
-                    });
-                }
-                return sum;
-            }
-        },
-        methods: {
-            handleAmmount(qtd, index) {
-                this.items[index].ammount = qtd;
-                window.localStorage.setItem('order-bag', JSON.stringify(this.items));
-            },
-            getItems() {
-                this.items = JSON.parse(window.localStorage.getItem('order-bag'));
-            },
-            getShopping() {
-                this.shopping = {
-                    name: "Shopping do seu zé",
-                    address: "Rua do seu zé"
-                }
-            },
-            getRestaurant() {
-                this.restaurant = {
-                    name: "McDonalds",
-                    time: "30-40"
-                }
-            }
-        }
+import restaurantItem from "./BagItem.vue";
+export default {
+  components: {
+    restaurantItem
+  },
+  data() {
+    return {
+      shopping: {},
+      //  shopping: [],
+      restaurant: {},
+      items: []
+      //    id: this.cnpj
     };
+  },
+  created() {
+    this.getShopping();
+    this.getRestaurant();
+    this.getItems();
+    //  this.getShoppingId();
+  },
+  //   props: {
+  //     cnpj: {
+  //       required: true,
+  //       type: String
+  //     }
+  //   },
+  computed: {
+    total: function() {
+      let sum = 0;
+      if (this.items) {
+        this.items.forEach(item => {
+          sum += item.value * item.ammount;
+        });
+      }
+      return sum;
+    }
+  },
+  methods: {
+    handleAmmount(qtd, index) {
+      this.items[index].ammount = qtd;
+      window.localStorage.setItem("order-bag", JSON.stringify(this.items));
+    },
+    // handleCnpj(cnpj, index) {
+    //   this.shopping[index].cnpj = id;
+    //   window.localStorage.setItem("shopping", JSON.stringify(this.shopping));
+    // },
+    getItems() {
+      this.items = JSON.parse(window.localStorage.getItem("order-bag"));
+    },
+    getShopping() {
+      this.shopping = {
+        name: "Shopping do seu zé",
+        address: "Rua do seu zé",
+        cnpj: "123456789"
+      };
+    },
+    // getShoppingId() {
+    //   this.shopping = JSON.parse(window.localStorage.getItem("shopping"));
+    // },
+    getRestaurant() {
+      this.restaurant = {
+        name: "McDonalds",
+        time: "30-40"
+      };
+    }
+  }
+};
 </script>
 
 <style lang="scss">
 .order-bag {
-    .v-bottom-navigation {
-        position: sticky;
-        bottom: 0;
-    }
+  .v-bottom-navigation {
+    position: sticky;
+    bottom: 0;
+  }
 }
 
 .v-content__wrap {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
 .card {
+  width: 100%;
+  background-color: $c-white;
+  box-shadow: 0px 4px 10px #00000033;
+  padding-top: 20px;
+  margin-bottom: 15px;
+
+  &__image-area {
+    width: 20%;
+    height: 100%;
+    float: left;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &__image {
+      height: 40px;
+    }
+  }
+
+  &__text-area {
+    width: 80%;
+    float: right;
+    box-sizing: border-box;
+    padding-right: 10px;
+
+    &__text {
+      border-bottom: 1px solid #c7c7c7;
+    }
+
+    &__button {
+      display: flex;
+      justify-content: center;
+      font-weight: bold;
+      font-size: 12px;
+      padding-top: 10px;
+      padding-bottom: 10px;
+
+      a:link,
+      a:visited {
+        text-decoration: none;
+        color: #797979;
+      }
+    }
+  }
+
+  &__restaurant {
+    border-bottom: 1px solid #c7c7c7;
+    padding: 0 10px 25px 10px;
+
+    p {
+      font-size: 14px;
+    }
+  }
+
+  &__price {
+    font-size: 14px;
+    font-weight: 25px;
+    padding: 5px 10px 0px 10px;
+
     width: 100%;
-    background-color: $c-white;
-    box-shadow: 0px 4px 10px #00000033;
-    padding-top: 20px;
-    margin-bottom: 15px;
+    text-align: center;
 
-    &__image-area {
-        width: 20%;
-        height: 100%;
-        float: left;
+    a:link,
+    a:visited {
+      text-decoration: none;
+      width: 100%;
+      color: #ef596b;
+    }
+
+    &__total {
+      display: flex;
+      justify-content: space-between;
+      font-size: 16px;
+      font-weight: normal;
+    }
+  }
+
+  &__payment-method {
+    width: 100%;
+    padding-left: 10px;
+
+    &__title {
+      font-size: 18px;
+      font-weight: normal;
+      border-bottom: 1px solid #c7c7c7;
+    }
+
+    &__credit {
+      display: flex;
+      justify-content: space-between;
+      padding-right: 10px;
+
+      p {
+        font-size: 12px;
+        color: #797979;
+        margin-top: 16px;
+      }
+
+      a:visited,
+      a:link {
+        font-size: 12px;
+        text-decoration: none;
+        color: #000000;
         display: flex;
-        justify-content: center;
         align-items: center;
-        
-        &__image {
-            height: 40px;
-        }
+      }
+      border-bottom: 1px solid#C7C7C7;
     }
 
-    &__text-area{
-        width: 80%;
-        float: right;
-        box-sizing: border-box;
-        padding-right: 10px;
+    &__cpf {
+      padding-top: 12px;
+      padding-bottom: 12px;
 
-        &__text{
-            border-bottom: 1px solid #c7c7c7;
-        }
-
-        &__button{
-            display: flex;
-            justify-content: center;
-            font-weight: bold;
-            font-size: 12px;
-            padding-top: 10px;
-            padding-bottom: 10px;
-
-            a:link, a:visited {
-                text-decoration: none;
-                color: #797979;
-            }
-        }
+      a:link,
+      a:visited {
+        text-decoration: none;
+        font-size: 12px;
+        color: #797979;
+      }
     }
-
-    &__restaurant {
-        border-bottom: 1px solid #c7c7c7;
-        padding: 0 10px 25px 10px;
-
-        p {
-            font-size: 14px;
-        }
-    }
-
-    &__price{        
-
-        font-size: 14px;
-        font-weight: 25px;
-        padding: 5px 10px 0px 10px;
-
-        width: 100%;
-        text-align: center; 
-
-        a:link, a:visited {
-            text-decoration: none;
-            width: 100%;
-            color: #EF596B;
-        }
-
-        &__total {
-            display: flex;
-            justify-content: space-between;
-            font-size: 16px;
-            font-weight: normal;
-        }
-    }
-
-    &__payment-method {
-        width: 100%;
-        padding-left: 10px;
-
-
-        &__title {
-            font-size: 18px;
-            font-weight: normal;
-            border-bottom: 1px solid #C7C7C7;
-        }
-
-        &__credit {
-            display: flex;
-            justify-content: space-between;
-            padding-right: 10px;
-
-            p {
-                font-size: 12px;
-                color: #797979;
-                margin-top: 16px;
-            }
-
-            a:visited, a:link{
-                font-size: 12px;
-                text-decoration: none;
-                color: #000000;
-                display: flex;
-                align-items: center;
-            }
-            border-bottom: 1px solid#C7C7C7;
-        }
-
-        &__cpf {
-            padding-top: 12px;
-            padding-bottom: 12px;
-
-            a:link, a:visited {
-                text-decoration: none;
-                font-size: 12px;
-                color: #797979;
-            }
-        }
-    }
+  }
 }
 
-.v-bottom-navigation{
-    position: fixed;
-    bottom: 0;
-    left: 0;
+.v-bottom-navigation {
+  position: fixed;
+  bottom: 0;
+  left: 0;
 }
 
 #shopping-name {
-    margin-bottom: 0px !important;
-    font-size: 14px;
+  margin-bottom: 0px !important;
+  font-size: 14px;
 }
 
-#localization{
-    font-size: 12px;
-    color: #797979;
+#localization {
+  font-size: 12px;
+  color: #797979;
 }
 
-#restaurant-title{
-    font-size: 22px !important;
+#restaurant-title {
+  font-size: 22px !important;
 }
 
-#foods{
-    padding-top: 10px;
+#foods {
+  padding-top: 10px;
 }
 
-#payment{
-    padding-top: 5px;
+#payment {
+  padding-top: 5px;
 }
 </style>
