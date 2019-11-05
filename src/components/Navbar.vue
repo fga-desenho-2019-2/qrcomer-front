f8e89bf85ba4b117422f33717c0c95cdc613fc4b
MatheusBlanco <msallesblanco@gmail.com>
2019-11-05 19:45:19 -0300

    Removing duplicate code and using header from navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,117 +1,110 @@
 <template>
-    <v-app-bar 
-        app
-        elevate-on-scroll 
-        v-scroll="floatingNav"
-        id="landing-page-header">
-        <v-app-bar-nav-icon class="d-flex d-sm-flex d-md-none d-lg-none text-shadow landing-page-header--icon"></v-app-bar-nav-icon>
-        <img 
-            v-if="white" 
-            src="~@/assets/images/QRComer.png" 
-            alt="Logo" 
-            class="brand pr-5 d-none d-sm-none d-md-flex d-lg-flex">
-        <img 
-            v-else 
-            src="~@/assets/images/logo.svg" 
-            alt="Logo" 
-            class="brand pr-5 d-none d-sm-none d-md-flex d-lg-flex">
-        <v-toolbar-title class="text-uppercase d-none d-sm-none d-md-flex d-lg-flex text-shadow">
-            <span>QR</span>
-            <span class="font-weight-light">Comer</span>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn
-            text
-            tile
-            href="#">
-            <span class="mr-2 text-shadow">Login</span>
-        </v-btn>
-        <v-btn
-            text
-            tile
-            href="#">
-            <span class="mr-2 text-shadow">Cadastrar</span>
-        </v-btn>
-    </v-app-bar>
+  <v-app-bar app elevate-on-scroll v-scroll="floatingNav" id="landing-page-header">
+    <v-app-bar-nav-icon
+      class="d-flex d-sm-flex d-md-none d-lg-none text-shadow landing-page-header--icon"
+    ></v-app-bar-nav-icon>
+    <img
+      v-if="white"
+      src="~@/assets/images/QRComer.png"
+      alt="Logo"
+      class="brand pr-5 d-none d-sm-none d-md-flex d-lg-flex"
+    />
+    <img
+      v-else
+      src="~@/assets/images/logo.svg"
+      alt="Logo"
+      class="brand pr-5 d-none d-sm-none d-md-flex d-lg-flex"
+    />
+    <v-toolbar-title class="text-uppercase d-none d-sm-none d-md-flex d-lg-flex text-shadow">
+      <span>QR</span>
+      <span class="font-weight-light">Comer</span>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn text tile @click="routeTo({'path': '/auth', 'query': {'loginType':true}})" href="#">
+      <span class="mr-2 text-shadow">Login</span>
+    </v-btn>
+    <v-btn text tile @click="routeTo({'path': '/auth', 'query': {'loginType':false}})" href="#">
+      <span class="mr-2 text-shadow">Cadastrar</span>
+    </v-btn>
+  </v-app-bar>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            white: false,
-        }
-    },
-    methods: {
-        floatingNav() {
-            if (window.scrollY > 5) {
-                this.white = true
-            } else {
-                this.white = false
-            }
+import { routeTo } from "../services/context";
 
-        }
-    }
-}
+export default {
+  data() {
+    return {
+      white: false
+    };
+  },
+  methods: {
+    floatingNav() {
+      if (window.scrollY > 5) {
+        this.white = true;
+      } else {
+        this.white = false;
+      }
+    },
+    routeTo
+  }
+};
 </script>
 
 <style lang="scss">
 #landing-page-header {
-    background: transparent;
-    transition: all 0.3s ease;
-    z-index: 999;
-    color: $c-white;
+  background: transparent;
+  transition: all 0.3s ease;
+  z-index: 999;
+  color: $c-white;
 
-    &.v-app-bar--is-scrolled {
-        background: white;
-        color: $c-gray90;
+  &.v-app-bar--is-scrolled {
+    background: white;
+    color: $c-gray90;
 
-        .v-btn {
-            color: $c-gray90;  
-            
-            &.landing-page-header--icon {
-                color: $main-color;
-            }
-        }
-
-        .text-shadow {
-            text-shadow: none;
-        }
-
-        .brand {
-            filter: none;
-        }
-    }
-
-    
-    
-    .brand {
-        height: 50%;
-        object-fit: contain;
-        filter: drop-shadow(1px 2px 1px $c-gray70)
-    }
-    
-    .v-toolbar__title {
-        font-size: 24px;
-        font-weight: 400;
-        line-height: 2rem;
-    }
-    
     .v-btn {
-        color: $c-white;
-        text-transform: initial;
+      color: $c-gray90;
+
+      &.landing-page-header--icon {
+        color: $main-color;
+      }
     }
 
     .text-shadow {
-        text-shadow: 1px 2px 4px $c-gray70;
+      text-shadow: none;
     }
 
-    @media screen and (max-width: 1920px) {
-        .v-toolbar__content {
-            max-width: 1300px;
-            margin: 0px auto;
-        }
+    .brand {
+      filter: none;
     }
+  }
 
+  .brand {
+    height: 50%;
+    object-fit: contain;
+    filter: drop-shadow(1px 2px 1px $c-gray70);
+  }
+
+  .v-toolbar__title {
+    font-size: 24px;
+    font-weight: 400;
+    line-height: 2rem;
+  }
+
+  .v-btn {
+    color: $c-white;
+    text-transform: initial;
+  }
+
+  .text-shadow {
+    text-shadow: 1px 2px 4px $c-gray70;
+  }
+
+  @media screen and (max-width: 1920px) {
+    .v-toolbar__content {
+      max-width: 1300px;
+      margin: 0px auto;
+    }
+  }
 }
 </style>
--- src/views/LandingPage/Home.vue
+++ src/views/LandingPage/Home.vue
@@ -1,48 +1,47 @@
 <template>
-    <v-content class="d-flex flex-column pt-0">
-        <LandingPageHeader></LandingPageHeader>
+  <v-content class="d-flex flex-column pt-0">
+    <LandingPageHeader></LandingPageHeader>
 
-        <div>
-            <QrcBanner/>
+    <div>
+      <QrcBanner />
 
-            <QrcSobre/>
+      <QrcSobre />
 
-            <QrcFuncionamento/>
-            
-            <QrcTime/>
-        </div>
-        <QrcFooter/>
-    </v-content>
+      <QrcFuncionamento />
+
+      <QrcTime />
+    </div>
+    <QrcFooter />
+  </v-content>
 </template>
 
 <script>
-import Footer from './Footer.vue'
-import Banner from './Banner.vue'
-import Time from './Time.vue'
-import Funcionamento from './Funcionamento.vue'
-import Sobre from './Sobre.vue'
-import LandingPageHeader from './Header.vue'
+import Footer from "./Footer.vue";
+import Banner from "./Banner.vue";
+import Time from "./Time.vue";
+import Funcionamento from "./Funcionamento.vue";
+import Sobre from "./Sobre.vue";
+import LandingPageHeader from "../../components/Navbar";
 
 export default {
-    components: {
-        "QrcBanner": Banner,
-        "QrcFuncionamento": Funcionamento,
-        "QrcTime": Time,
-        "QrcFooter": Footer,
-        "QrcSobre": Sobre,
-        "LandingPageHeader": LandingPageHeader
-    }
-}
+  components: {
+    QrcBanner: Banner,
+    QrcFuncionamento: Funcionamento,
+    QrcTime: Time,
+    QrcFooter: Footer,
+    QrcSobre: Sobre,
+    LandingPageHeader: LandingPageHeader
+  }
+};
 </script>
 
 <style lang="scss">
 .v-content {
-    background: $c-white;
-    
-    .v-content__wrap {
-        display: flex;
-        flex-direction: column;
-    }
-}
+  background: $c-white;
 
+  .v-content__wrap {
+    display: flex;
+    flex-direction: column;
+  }
+}
 </style>
