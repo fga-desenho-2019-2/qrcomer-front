93190607bb40023d9660689be80f5fc9324eb7d7
phe0 <pheofo@gmail.com>
2019-11-06 15:22:46 -0300

    add list card page and card item component

---
+++ src/views/Cards/CardItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card-item">
+    <div class="card-item__text">
+        <p class="mb-0" >Cartão de crédito</p>
+        <p class="mb-0" id="card-number" >{{card.number}}</p>
+    </div>
+    <img class="card-item__image" src="../../assets/images/credit_card.svg"/>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        card: {
+            required: true,
+            type: Object
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+
+.card-item {
+    height: 100px;
+    border-bottom: 1px solid $c-gray40;
+    display: grid;
+    grid-template-columns: 1fr 50px;
+    align-items: center;
+}
+
+#card-number {
+    color: $c-gray50;
+}
+</style>
---
+++ src/views/Cards/CardList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="cards-list">
+    <div class="cards-list__explanation">
+      <p class="mb-0">
+        Escolha uma forma de pagamento 
+      </p>
+    </div>
+    <div>
+      <CardItem v-for="(card, index) in cards" :key="card.number+index" :card="card" />
+    </div>
+  </div>
+</template>
+
+<script>
+import CardItem from './CardItem'
+
+export default {
+  components: {
+    CardItem
+  },
+  props: {
+    cards: {
+        required: true
+    }
+  },
+  created() {
+      console.log(this.cards)
+  } 
+};
+</script>
+
+<style lang="scss">
+.cards-list {
+
+  padding: 10px;
+
+  &__explanation {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100px;
+    width: 100%;
+    border-bottom: 1px solid $c-gray40;
+    color: $c-gray60;
+  }
+}
+
+</style>
