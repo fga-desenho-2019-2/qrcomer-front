e21552ba1e620862f1a164c9a06c4a577a0a37eb
phe0 <pheofo@gmail.com>
2019-11-08 00:01:52 -0300

    add credit card representation

--- src/views/Cards/CreateCard.vue
+++ src/views/Cards/CreateCard.vue
@@ -1,6 +1,14 @@
 <template>
   <v-content class="create-card-page d-flex flex-column pt-0">
     <div>
+      <div class="credit-card-area">
+        <div class="credit-card-area__card"> 
+          <img class="credit-card-area__card__chip" src="../../assets/images/chip.svg"/>
+          <h2 class="credit-card-area__card__number mb-0">{{creditCard.number}}</h2>
+          <p class="credit-card-area__card__date mb-3"><span id="valid">Validade</span> {{creditCard.validation}}</p>
+          <h3 class="credit-card-area__card__name mb-0">{{nameUpperCase}}</h3>
+        </div>
+      </div>
       <v-form ref="form">
         <v-container>
           <v-row>
@@ -13,7 +21,7 @@
             </v-col>
 
             <v-col cols="8" md="1" class="pb-0">
-              <v-text-field type="date" v-model="creditCard.validation" label="Data de Validade" color="#e18855"></v-text-field>
+              <v-text-field type="number" v-model="creditCard.validation" label="Data de Validade" color="#e18855"></v-text-field>
             </v-col>
 
             <v-col cols="4" md="1" class="pb-0">
@@ -60,6 +68,14 @@ export default {
       this.newCreditCard = this.creditCard;
     }
   },
+  computed: {
+    nameUpperCase: function () {
+      if(this.creditCard.holderName)
+        return this.creditCard.holderName.toUpperCase();
+      else 
+        return ''
+    }
+  },
   methods: {
     copyCreditCard: function () {
       this.newCreditCard = this.creditCard;
@@ -78,6 +94,54 @@ export default {
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css?family=Lexend+Deca|Palanquin:100,300,400,500,700&display=swap");
+
+.credit-card-area {
+  height: 60vw;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 10px 0 10px;
+
+  &__card {
+    width: 100%;
+    max-width: 500px;
+    height: 100%;
+    max-height: 300px;
+    background-image: linear-gradient(to bottom right, $main-color , $second-color);
+    border-radius: 10px;
+    color: $c-white;
+    padding: 15px 15px 15px 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    box-shadow: 0 4px 4px #00000040;
+
+    &__chip {
+      align-self: flex-start;
+      margin-bottom: 15px;
+    }
+
+    &__number {
+      height: 36px;
+      font-size: 24px !important;
+    }
+
+    &__date {
+      height: 21px;
+      font-size: 14px !important;
+    }
+
+    &__name {
+      height: 21px;
+      font-size: 14px !important;
+    }
+  }
+}
+
+#valid {
+  color: #975C3A;
+}
 
 .create-card-page .v-item-group.v-bottom-navigation .v-btn {
   max-width: none;
